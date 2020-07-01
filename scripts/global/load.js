11553ddfb143c7f295601f72e1f6bff29d513a54
Marcus <marcus-goncalves@outlook.com.br>
2020-06-30 21:40:34 -0300

    bugs fixed

--- scripts/global/load.js
+++ scripts/global/load.js
@@ -1,22 +1,22 @@
 function preload() {
-    telaInicialImage = loadImage("/assets/scenario/telaInicial.png")
-    fontTelaInicial = loadFont("/assets/others/fontTelaInicial.ttf")
+    telaInicialImage = loadImage("./assets/scenario/telaInicial.png")
+    fontTelaInicial = loadFont("./assets/others/fontTelaInicial.ttf")
 
-    scenarioImage1 = loadImage("/assets/scenario/layers/far-buildings.png");
-    scenarioImage2 = loadImage("/assets/scenario/layers/back-buildings.png");
-    scenarioImage3 = loadImage("/assets/scenario/layers/foreground.png");
+    scenarioImage1 = loadImage("./assets/scenario/layers/far-buildings.png");
+    scenarioImage2 = loadImage("./assets/scenario/layers/back-buildings.png");
+    scenarioImage3 = loadImage("./assets/scenario/layers/foreground.png");
   
-    characterImage = loadImage("/assets/character/run.png");
-    enemyImage = loadImage("/assets/enemies/v-police.png");
-    enemy2Image = loadImage("/assets/enemies/v-red.png");
-    enemy3Image = loadImage("/assets/enemies/v-truck.png")
-    enemy4Image = loadImage("/assets/enemies/v-yellow.png")
-    lifeImage = loadImage("/assets/others/heart.png")
+    characterImage = loadImage("./assets/character/run.png");
+    enemyImage = loadImage("./assets/enemies/v-police.png");
+    enemy2Image = loadImage("./assets/enemies/v-red.png");
+    enemy3Image = loadImage("./assets/enemies/v-truck.png")
+    enemy4Image = loadImage("./assets/enemies/v-yellow.png")
+    lifeImage = loadImage("./assets/others/heart.png")
 
-    gameOverImage = loadImage("/assets/others/gameOver.png");
+    gameOverImage = loadImage("./assets/others/gameOver.png");
   
-    soundOfGame = loadSound("/sounds/gameTrack.mp3");
-    soundOfJump = loadSound("/sounds/jump.mp3");
-    soundOfDamage = loadSound("/sounds/damage.wav")
-    soundOfGameOver = loadSound("/sounds/gameOver.mp3");
+    soundOfGame = loadSound("./sounds/gameTrack.mp3");
+    soundOfJump = loadSound("./sounds/jump.mp3");
+    soundOfDamage = loadSound("./sounds/damage.wav")
+    soundOfGameOver = loadSound("./sounds/gameOver.mp3");
   }
